f910ce233295eb27819635888f6dbc2df45a53e2
Abdul Wasay Ali <pc.16752abdulwasayali@gmail.com>
2025-12-04 02:27:37 +0500

    Add files via upload

---
+++ assi1 js/app.js
@@ -0,0 +1,120 @@
+  //Assignment # 1//
+
+ // Question # 1 //
+ alert ('welcome to my website!');
+
+ //Question # 2//
+  alert("Hello! This is a message on your webpage.");
+
+  //Question # 3//
+  alert("Welcome to JavaScript!\nThis message has a line break.");
+
+  //Question # 4//
+  alert("First Message");
+  alert("Second Message");
+  alert("Third Message");
+  alert("Fourth Message");
+
+  //Question # 5//
+  console.log("Hello! This message is shown in the browser console.");
+
+ // Assignment #2 //
+
+ //Question # 1//
+
+ var username;
+
+ //Question # 2//
+
+ var myName = "your full name";
+
+ //Question # 3//
+
+ var message;
+ message = "Hello World";
+ alert(message);
+
+ //Question # 4//
+
+ var studentName = "Abdul Wasay";
+ var age = "22 years old";
+ var course = "Web Development";
+
+ alert(studentName);
+ alert(age);
+ alert(course);
+
+ //Question # 5//
+ var food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+ alert(food);
+
+ //Question # 6//
+ var email = "pc.16752abdulwasay@gmail.com";
+ alert("My email address is " + email);
+
+
+ //Question # 7//
+ var book = "A smarter way to learn JavaScript";
+ alert("I am trying to learn from the book " + book);
+
+ //Question # 8//
+ 
+ document.write("Yah! I can write HTML content through JavaScript");
+
+  //Question # 9//
+  var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+  alert(design);
+  document.write(design);
+  
+  // Assignment #3 //
+ 
+ //Question # 1//
+ var age = 22;
+ alert("I am " + age + " years old");
+
+
+ //Question # 2//
+ var visitCount = 7;   
+ document.write("You have visited this site " + visitCount + " times");
+
+ //Question # 3//
+
+ var birthYear = 2003;   
+ document.write("My birth year is " + birthYear + "<br>");
+ document.write("Data type of my declared variable is number");
+
+ //Question # 4//
+ var visitorName = "Abdul wasay ";
+ var productTitle = "T-shirt(s)";
+ var quantity = 7;
+ document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store.");
+
+  // Assignment #4 //
+  //Question # 1//
+ var name, age, city;
+
+  //Question # 2//
+//   Legal
+  var myName;
+  var $price;
+  var _age;
+  var user1;
+  var my_variable;
+  //   illegalvar       
+  var my-name;      // hyphen not allowed
+  var var;          // reserved word
+  var my variable;  // space not allowed
+  var @item;        // special character not allowed
+  
+    //Question # 3//
+  document.write("<h1>Rules for naming JS variables</h1>");
+
+  document.write("Variable names can only contain letters, numbers, $ . For example: $my 1stVariable <br>");
+  
+  document.write("Variables must begin with a letter, $ . For example: $name, name or name <br>");
+  
+  document.write("Variable names are case sensitive <br>");
+  
+  document.write("Variable names should not be JS keywords");
+  
+    
