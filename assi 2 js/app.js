cfe8d4cc78b749c54be47a92bf8bd85ac545b922
Abdul Wasay Ali <pc.16752abdulwasayali@gmail.com>
2025-12-12 21:21:51 +0500

    Add files via upload

---
+++ assi 2 js/app.js
@@ -0,0 +1,34 @@
+  //Chapter 6 Till 9//
+
+  //Question # 1//
+ var a = 10;
+  document.write("Result:<br>");
+  document.write("The value of a is: " + a + "<br><br>");
+  
+  document.write("The value of ++a is: " + (++a) + "<br>");
+  document.write("Now the value of a is: " + a + "<br><br>");
+  
+  document.write("The value of a++ is: " + (a++) + "<br>");
+  document.write("Now the value of a is: " + a + "<br><br>");
+  
+  document.write("The value of --a is: " + (--a) + "<br>");
+  document.write("Now the value of a is: " + a + "<br><br>");
+  
+  document.write("The value of a-- is: " + (a--) + "<br>");
+  document.write("Now the value of a is: " + a + "<br><br>");
+
+
+
+  //Question # 2//
+
+
+
+
+  //Question # 3//
+
+
+
+
+
+
+  //Question # 4//
