04f3032d0a52ac6e20752510e3ece002ccd6a89d
Abdul Wasay Ali <pc.16752abdulwasayali@gmail.com>
2026-01-14 21:06:51 +0500

    Add files via upload

---
+++ assi 8 js/app.js
@@ -0,0 +1,89 @@
+//  chapter 31-34
+
+// question 1
+
+// var now = new Date();
+// console.log(now);
+
+// question 2
+
+// var months = ["January","February","March","April","May","June","July","August","September","Octuber","November","December"];
+// var now = new Date();
+// console.log(months[now.getMonth("December")]);
+
+
+// question 3
+
+var days = ["sun","mon","tue","wed","thu","fri","sat"];
+var day = new Date();
+var theday = now.getday();
+console.log(days[theDay]);
+
+// question 4
+
+// var days = new Date() .getDay();
+// if (day === 0 || day === 6) {
+//     console.log("it's Fun day")
+// } 
+
+
+
+
+
+
+
+
+// question 1
+
+
+
+
+
+
+
+
+
+
+// question 1
+
+
+
+
+
+
+
+
+// question 1
+
+
+
+
+
+
+
+
+// question 1
+
+
+
+
+
+
+// question 1
+
+
+
+
+
+
+// question 1
+
+
+
+
+
+
+
+// question 1
+
+
