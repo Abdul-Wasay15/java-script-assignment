e88b09fc2bd611271f86eb7f19e747b42aa9eb37
Abdul Wasay Ali <pc.16752abdulwasayali@gmail.com>
2026-01-15 19:45:02 +0500

    Add files via upload

---
+++ assi 9 js/app.js
@@ -0,0 +1,64 @@
+// chapter 35 to 38 
+// question 1
+// function showDateTime() {
+//   console.log(new Date());
+// }
+// showDateTime();
+
+// question 2
+
+// function greetUser(firstName, lastName) {
+//   console.log("Hello " + firstName + " " + lastName);
+// }
+// greetUser("Ali", "Khan");
+
+// question 3
+
+function addnumbers(a , b){
+     return a + b};
+console.log(addnumbers(3,5));
+
+
+
+// question 4
+
+
+
+
+
+// question 5
+
+
+
+
+
+
+// question 6
+
+
+
+
+
+
+// question 7
+
+
+
+
+
+
+// question 8
+
+
+
+
+
+
+// question 9
+
+
+
+
+
+
+// question 10
